61695f6ec5a4e354223b5e47bf9de149bab6ec7e
ANIIAN91 <ANIIAN91@user.noreply.github.com>
2024-11-21 19:23:16 +0800

    Initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+  title: "ANIIAN'S DIARY", // 网站标题
+  description: "Record the learning of relevant deep learning, Linux, shell, etc.", // 网站描述
+
+  themeConfig: {
+    // 主题级选项
+    nav: [
+      { text: '首页', link: '/index' }, // 对应 docs/about.md
+      {
+        text: '记录',
+        items: [
+          { text: "Linux", link: "/linux/" },    // 对应 docs/linux/index.md
+          { text: "Shell", link: "/shell/" },     // 对应 docs/shell/index.md
+          { text: "深度学习", link: "/depp-learning/" }, // 对应 docs/deep-learning/index.md
+        ]
+      }
+    ],
+  },
+})
---
+++ docs/shell/index.md
@@ -0,0 +1,2 @@
+1. [安装vitepress](../linux/20241121.md)
+
