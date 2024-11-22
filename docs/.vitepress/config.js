ff1b7e51fcc2c3f1d3b56add23f5294640138b3a
ANIIAN91 <ANIIAN91@user.noreply.github.com>
2024-11-22 20:34:01 +0800

    update env and many document  for 'origin/main'

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,11 +1,116 @@
 import { defineConfig } from 'vitepress'
+import mathjax3 from 'markdown-it-mathjax3'
+const customElements = [
+  'math',
+  'maction',
+  'maligngroup',
+  'malignmark',
+  'menclose',
+  'merror',
+  'mfenced',
+  'mfrac',
+  'mi',
+  'mlongdiv',
+  'mmultiscripts',
+  'mn',
+  'mo',
+  'mover',
+  'mpadded',
+  'mphantom',
+  'mroot',
+  'mrow',
+  'ms',
+  'mscarries',
+  'mscarry',
+  'mscarries',
+  'msgroup',
+  'mstack',
+  'mlongdiv',
+  'msline',
+  'mstack',
+  'mspace',
+  'msqrt',
+  'msrow',
+  'mstack',
+  'mstack',
+  'mstyle',
+  'msub',
+  'msup',
+  'msubsup',
+  'mtable',
+  'mtd',
+  'mtext',
+  'mtr',
+  'munder',
+  'munderover',
+  'semantics',
+  'math',
+  'mi',
+  'mn',
+  'mo',
+  'ms',
+  'mspace',
+  'mtext',
+  'menclose',
+  'merror',
+  'mfenced',
+  'mfrac',
+  'mpadded',
+  'mphantom',
+  'mroot',
+  'mrow',
+  'msqrt',
+  'mstyle',
+  'mmultiscripts',
+  'mover',
+  'mprescripts',
+  'msub',
+  'msubsup',
+  'msup',
+  'munder',
+  'munderover',
+  'none',
+  'maligngroup',
+  'malignmark',
+  'mtable',
+  'mtd',
+  'mtr',
+  'mlongdiv',
+  'mscarries',
+  'mscarry',
+  'msgroup',
+  'msline',
+  'msrow',
+  'mstack',
+  'maction',
+  'semantics',
+  'annotation',
+  'annotation-xml',
+  'mjx-container',
+  'mjx-assistive-mml',
+];
 
-// https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
   base: '/',
   title: "ANIIAN'S DIARY", // 网站标题
   description: "Record the learning of relevant deep learning, Linux, shell, etc.", // 网站描述
+  markdown: {
+    config: (md) => {
+      md.use(mathjax3);
+    }
+  },
+  vue: {
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => customElements.includes(tag),
+      },
+    },
+  },
 
+  pageNav: true,  // 启用上一页和下一页功能
+  returnHome: true,  // 启用返回首页按钮
+  sidebarCollapsible: true,  // 启用侧边栏折叠
+  lastUpdated: true,
   themeConfig: {
     // 主题级选项
     nav: [
@@ -22,9 +127,6 @@ export default defineConfig({
       prev: '上一页',
       next: '下一页'
     },
-    pageNav: true,  // 启用上一页和下一页功能
-    returnHome: true,  // 启用返回首页按钮
-    sidebarCollapsible: true,  // 启用侧边栏折叠
-    lastUpdated: true,
+
   },
 })
--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,6 @@
 # **采菊东篱下，悠然见南山**
 
 
-test
+[Earthworm](https://julebu.co/)     
+[Follow](https://app.follow.is/feeds/all/pending?view=0)        
+[CCF](https://passport.ccf.org.cn/sso/platform?_access_code=b34a144c6fd94a4184b3c4296ee5ff94)
